e4dbebb3f4fec4698c1d603b5d669d18063fd75e
Daniel Vieira <danielvnso7@gmail.com>
2024-06-07 14:32:07 -0300

    Footer, Body e demais paginas

--- script.js
+++ script.js
@@ -8,3 +8,22 @@ function menuShow(){
         document.querySelector('.icon').src = "img/close_white.png";
     }
 }
+
+function entrar(event) {
+
+    var login = document.getElementById("login").value;
+    var senha = document.getElementById("senha").value;
+
+    if (login === "1234" && senha === "1234") {
+        alert("logado com sucesso");
+        window.location.href = '../index.html';
+
+    }
+    else {
+        alert("Usuário ou senha inválidos");
+    }
+}
+
+function voltar(){
+    window.location.href = '../index.html'
+}
